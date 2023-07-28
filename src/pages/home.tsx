3fba4a78413d57e51f20f2bbaeebd18e83d7ad4d
CHEN <812607138@qq.com>
2023-07-28 17:56:09 +0800

    add grid layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -3,7 +3,13 @@ import './home.scss';
 
 export const Home = () => {
   return (
-    <div className="wrapper">
+    <div className="home">
+      <header></header>
+      <main>
+        <section className="section1"></section>
+        <section className="section2"></section>
+        <section className="section3"></section>
+      </main>
     </div>
   );
 };
