8b4fd63bc9f080e4c1520f415811a19604123897
CHEN <812607138@qq.com>
2023-07-29 19:02:30 +0800

    optimize border style

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 export const Form = () => {
   return (
-    <div>hi</div>
+    <div className="formWrapper">
+      <div><p>1,365</p>重点单位数</div>
+      <div><p>8,227</p>消防隐患单位数</div>
+      <div><p>851,369</p>全年消防报警总数</div>
+    </div>
   );
 };
